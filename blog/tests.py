266a49d65b35b67ad43e34461205c334b280a162
canlelai <canlelai@outlook.com>
2020-08-30 13:46:41 +0100

    Add CV page.

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,20 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from blog.views import post_list
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, post_list)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_post_list_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.strip().startswith('<html>'))
+        self.assertIn('<title>Canle Blog</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
